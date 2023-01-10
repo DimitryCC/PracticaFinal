9ebf059684329541416dfffcad9485dea82a9877
dimitry <dimitrycompany@paucasesnovescifp.cat>
2023-01-10 16:59:42 +0100

    He creado Cosas

--- ProyectoFinal/app/Http/Controllers/ServicioControler.php
+++ ProyectoFinal/app/Http/Controllers/ServicioControler.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DescripcioneControler extends Controller
+class ServicioControler extends Controller
 {
     //
 }
--- ProyectoFinal/app/Models/Descripcion.php
+++ ProyectoFinal/app/Models/Descripcion.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Descripcione extends Model
+class Descripcion extends Model
 {
     use HasFactory;
 }
