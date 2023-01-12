c7a22f07891f76b5bfef38528be91850a4a938a2
Joan Ramis <joanramis1@paucasesnovescifp.cat>
2023-01-12 20:32:31 +0100

    creat swagger per Servicio

--- ProyectoFinal/app/Http/Controllers/ServicioControler.php
+++ ProyectoFinal/app/Http/Controllers/ServicioControler.php
@@ -9,6 +9,43 @@ use Illuminate\Support\Facades\Validator;
 class ServicioControler extends Controller
 {
     //
+    /**
+     * Descripcion de un Servicio.
+     * @urlParam id integer required ID del alojamiento a mostrar.
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/servicio/{id}",
+     *     tags={"Servicios"},
+     *     summary="Mostrar un Servicio por ID",
+     *     @OA\Parameter(
+     *         description="Id del Servicio",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *         @OA\Examples(example="id", value="1", summary="Introduce el numero de ID del Servicio")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Informacion del Servicio.",
+     *          @OA\JsonContent(
+     *          @OA\Property(property="status", type="string", example="success"),
+     *          @OA\Property(property="data",type="object")
+     *           ),
+     *      ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hay un error.",
+     *         @OA\JsonContent(
+     *          @OA\Property(property="status", type="string", example="error"),
+     *          @OA\Property(property="data",type="string", example="Servicio no encontrado")
+     *           ),
+     *     )
+     * )
+     */
     public function show($id){
         try {
             $tupla = Servicio::findOrFail($id);
@@ -18,12 +55,60 @@ class ServicioControler extends Controller
         }
     }
 
-
+    /**
+     * Lista todos los Servicios.
+     *
+     *
+     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/servicio",
+     *     tags={"Servicios"},
+     *     summary="Mostrar todos los Servicios",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Mostrar todos los Servicios."
+     *     ),
+     * )
+     */
     public function tots(){
         $tuples= Servicio::paginate(10);
         return response()->json(['status'=>'success','result'=>$tuples],200);
     }
 
+    /**
+     * Borra un Servicio.
+     * @urlParam id integer required ID del Servicio a borrar.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * @OA\Delete(
+     *    path="/api/servicio/borra/{id}",
+     *    tags={"Servicios"},
+     *    summary="Borra un Servicio",
+     *    description="Borra un Servicio. Solo por Administradores",
+     *    security={{"bearerAuth":{}}},
+     *    @OA\Parameter(name="id", in="path", description="Id Servicio", required=true,
+     *        @OA\Schema(type="string")
+     *    ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="ID no encotrada")
+     *          ),
+     *       )
+     *      )
+     *  )
+     */
     public function borra($id){
         try {
             $tupla = Servicio::findOrFail($id)->delete();
@@ -33,6 +118,41 @@ class ServicioControler extends Controller
         }
     }
 
+    /**
+     * Crea un nuevo Servicio.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *    path="/api/servicio/crea",
+     *    tags={"Servicios"},
+     *    summary="Crea un Servicio",
+     *    description="Crea un Servicio. Solo por Administradores.",
+     *    security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           @OA\Property(property="NombreServicio", type="string", format="string", example="Esto es un nuevo Servicio"),
+     *        ),
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="Atributo obligatorio requerido")
+     *          ),
+     *       )
+     *  )
+     */
     public function crea(Request $request){
         $reglesvalidacio=[
             'NombreServicio'=>['required','max:100','unique:servicios,NombreServicio']
@@ -50,6 +170,54 @@ class ServicioControler extends Controller
         }
     }
 
+    /**
+     * Modificar un Servicio.
+     * @urlParam id integer required ID del Servicio.
+     * @bodyParam NombreServicio string Nombre del Servicio.
+     * @response scenario=success {
+     *  "status": "success",
+     * }
+     * @response status=400 scenario="validation error" {"status": "Validation error"}
+     */
+
+    /**
+     * Modificar un Servicio.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @OA\Put(
+     *    path="/api/servicio/modifica/{id}",
+     *    tags={"Servicios"},
+     *    summary="Modifica un Servicio",
+     *    description="Modifica un Servicio. Solo por Administradores.",
+     *    security={{"bearerAuth":{}}},
+     *    @OA\Parameter(name="id", in="path", description="Id Servicio", required=true,
+     *        @OA\Schema(type="string")
+     *    ),
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           @OA\Property(property="NombreServicio", type="string", format="string", example="Esto es un nuevo Servicio"),
+     *        ),
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="Atributo obligatorio requerido")
+     *          ),
+     *       )
+     *  )
+     */
     public function modifica(Request $request, $id){
         $tupla = Servicio::findOrFail($id);
         $reglesvalidacio=[
