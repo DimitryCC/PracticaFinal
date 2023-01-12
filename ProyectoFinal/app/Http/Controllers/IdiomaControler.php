34062eb17c9566782e0045d6f3680fda900f8fae
Joan Ramis <joanramis1@paucasesnovescifp.cat>
2023-01-12 20:08:23 +0100

    creat swagger per Idioma

--- ProyectoFinal/app/Http/Controllers/IdiomaControler.php
+++ ProyectoFinal/app/Http/Controllers/IdiomaControler.php
@@ -9,6 +9,43 @@ use Illuminate\Support\Facades\Validator;
 class IdiomaControler extends Controller
 {
     //
+    /**
+     * Descripcion de un Idioma.
+     * @urlParam id integer required ID del alojamiento a mostrar.
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/idioma/{id}",
+     *     tags={"Idiomas"},
+     *     summary="Mostrar un Idioma por ID",
+     *     @OA\Parameter(
+     *         description="id del Idioma",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *         @OA\Examples(example="id", value="1", summary="Introduce el numero de ID del Idioma")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Informacion del Idioma.",
+     *          @OA\JsonContent(
+     *          @OA\Property(property="status", type="string", example="success"),
+     *          @OA\Property(property="data",type="object")
+     *           ),
+     *      ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hay un error.",
+     *         @OA\JsonContent(
+     *          @OA\Property(property="status", type="string", example="error"),
+     *          @OA\Property(property="data",type="string", example="Idioma no encontrado")
+     *           ),
+     *     )
+     * )
+     */
     public function show($id){
         try {
             $tupla = Idioma::findOrFail($id);
@@ -18,12 +55,60 @@ class IdiomaControler extends Controller
         }
     }
 
-
+    /**
+     * Lista todos los Idiomas.
+     *
+     *
+     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/idioma",
+     *     tags={"Idiomas"},
+     *     summary="Mostrar todos los Idiomas",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Mostrar todos los Idiomas."
+     *     ),
+     * )
+     */
     public function tots(){
         $tuples= Idioma::paginate(10);
         return response()->json(['status'=>'success','result'=>$tuples],200);
     }
 
+    /**
+     * Borra un Idioma.
+     * @urlParam id integer required ID del Idioma a borrar.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * @OA\Delete(
+     *    path="/api/idioma/borra/{id}",
+     *    tags={"Idiomas"},
+     *    summary="Borra un Idioma",
+     *    description="Borra un Idioma. Solo por Administradores",
+     *    security={{"bearerAuth":{}}},
+     *    @OA\Parameter(name="id", in="path", description="Id Idioma", required=true,
+     *        @OA\Schema(type="string")
+     *    ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="ID no encotrada")
+     *          ),
+     *       )
+     *      )
+     *  )
+     */
     public function borra($id){
         try {
             $tupla = Idioma::findOrFail($id)->delete();
@@ -33,6 +118,41 @@ class IdiomaControler extends Controller
         }
     }
 
+    /**
+     * Crea un nuevo Idioma.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *    path="/api/idioma/crea",
+     *    tags={"Idiomas"},
+     *    summary="Crea un Idioma",
+     *    description="Crea un Idioma. Solo por Administradores.",
+     *    security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           @OA\Property(property="idioma", type="string", format="string", example="Esto es un nuevo Idioma"),
+     *        ),
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="Atributo obligatorio requerido")
+     *          ),
+     *       )
+     *  )
+     */
     public function crea(Request $request){
         $reglesvalidacio=[
             'idioma'=>['required','max:10','unique:idiomas,idioma']
@@ -50,6 +170,64 @@ class IdiomaControler extends Controller
         }
     }
 //
+    /**
+     * Modificar un Alojamiento.
+     * @urlParam id integer required ID del Alojamiento.
+     * @bodyParam nombre string Nombre del Alojamiento.
+     * @bodyParam adresa string Direccion del Alojamiento.
+     * @bodyParam numpero_personas number Numero de las max. personas del Alojamiento.
+     * @bodyParam numero_habitaciones number Numero de las habitaciones.
+     * @bodyParam numero_camas number Numero de las camas.
+     * @bodyParam numero_banos number Numero de los baños.
+     * @bodyParam tipo_alojamiento number Nivel de lujo del Alojamiento.
+     * @bodyParam tipo_vacacional number Tipo de Alojamiento vacacional.
+     * @bodyParam categoria number Categoria del Alojamiento.
+     * @bodyParam municipio number Municipio del Alojamiento.
+     * @bodyParam usuari number Usuario.
+     * @response scenario=success {
+     *  "status": "success",
+     * }
+     * @response status=400 scenario="validation error" {"status": "Validation error"}
+     */
+
+    /**
+     * Modificar un Idioma.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @OA\Put(
+     *    path="/api/alojamiento/idioma/{id}",
+     *    tags={"Idiomas"},
+     *    summary="Modifica un Idioma",
+     *    description="Modifica un Idioma. Solo por Administradores.",
+     *    security={{"bearerAuth":{}}},
+     *    @OA\Parameter(name="id", in="path", description="Id Categoria", required=true,
+     *        @OA\Schema(type="string")
+     *    ),
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           @OA\Property(property="idioma", type="string", format="string", example="Esto es un nuevo Idioma"),
+     *        ),
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="Atributo obligatorio requerido")
+     *          ),
+     *       )
+     *  )
+     */
     public function modifica(Request $request, $id){
         $tupla = Idioma::findOrFail($id);
         $reglesvalidacio=[
